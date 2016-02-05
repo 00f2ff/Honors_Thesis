47c675ae61ebc7a676f1ca3465e826f15a171bb1
Duncan McIsaac <dbmcisaac1@gmail.com>
2016-02-05 13:53:29 -0500

    added search and interactions (not a hover, just a keypress). Doesn't alert user of turning off Fingers though. I need to decide if that will be an instruction beforehand or not

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ var etsy = new Etsy();
 etsy.getRequest('listings', 'listings/trending', {'limit': 30});
 etsy.getRequest('categories', 'taxonomy/categories', {});
 // etsy.getRequest('listings', 'listings/active', {'limit': 30, 'category': 'candles'});
-// etsy.getRequest('listings', 'listings/active', {'limit': 30, 'keywords': 'birthday%20cards'}); // search will just replace spaces %20 and remove all other non-alphabetical characters
+
 // etsy.getRequest('sub-categories', 'taxonomy/categories/accessories', {});
 
 // Keycode arrays. Index of key corresponds with index of cell in hover-row
@@ -23,47 +23,63 @@ $('body').keydown(function(e) {
 	// find out whether UI is showing Table or ProductTable (because no arrow control / activation on latter)
 	// AND is located on select conditionals (does not apply to LinkList)
 	var isTable = $('.hover-row:nth-child(3)').children().length;
+	var isSearch = $('#search input:focus').length;
 	console.log(isTable);
-	console.log(kc);		
-	if (linkListKeyCodes.indexOf(kc) > -1) {
-		activateCategoryCell(kc);
-	} else if (tableFirstColumnKeyCodes.indexOf(kc) > -1 && isTable) {
-		activateTableCell(kc, 2);
-	} else if (tableSecondColumnKeyCodes.indexOf(kc) > -1 && isTable) {
-		activateTableCell(kc, 3);
-	} else if (tableThirdColumnKeyCodes.indexOf(kc) > -1 && isTable) {
-		activateTableCell(kc, 4);
-	} else if (kc === 37) { // left arrow
-		// Find which row is currently hovered over
-		// participants will need instruction on how hovering + arrow keys work
-		if ($('.hover-row:first-child:hover').length > 0) {
-			linkList.left();
-		} else if ($('.hover-row:not(:first-child):hover').length > 0 && isTable) { // table
-			table.left();
-		}
-	} else if (kc === 39) { // right arrow
-		if ($('.hover-row:first-child:hover').length > 0) {
-			linkList.right();
-		} else if ($('.hover-row:not(:first-child):hover').length > 0 && isTable) {
-			table.right();
-		}
-	} else if (kc === 192) { // grave accent (back button)
-		// reset categories in base case (from first page) or going back to first page
-		if (etsy.requestHistory.length === 1 || etsy.requestHistory.length === 2) {
-			// reset categories
-			while (linkList.previouslyActiveQueue.length > 0) {
+	console.log(kc);
+	// all key functionality doesn't hold for typing in search box
+	if (!isSearch) {
+		if (linkListKeyCodes.indexOf(kc) > -1) {
+			activateCategoryCell(kc);
+		} else if (tableFirstColumnKeyCodes.indexOf(kc) > -1 && isTable) {
+			activateTableCell(kc, 2);
+		} else if (tableSecondColumnKeyCodes.indexOf(kc) > -1 && isTable) {
+			activateTableCell(kc, 3);
+		} else if (tableThirdColumnKeyCodes.indexOf(kc) > -1 && isTable) {
+			activateTableCell(kc, 4);
+		} else if (kc === 37) { // left arrow
+			// Find which row is currently hovered over
+			// participants will need instruction on how hovering + arrow keys work
+			if ($('.hover-row:first-child:hover').length) {
 				linkList.left();
+			} else if ($('.hover-row:not(:first-child):hover').length && isTable) { // table
+				table.left();
 			}
+		} else if (kc === 39) { // right arrow
+			if ($('.hover-row:first-child:hover').length) {
+				linkList.right();
+			} else if ($('.hover-row:not(:first-child):hover').length && isTable) {
+				table.right();
+			}
+		} else if (kc === 192) { // grave accent (back button)
+			// reset categories in base case (from first page) or going back to first page
+			if (etsy.requestHistory.length === 1 || etsy.requestHistory.length === 2) {
+				// reset categories
+				while (linkList.previouslyActiveQueue.length) {
+					linkList.left();
+				}
+			}
+			if (etsy.requestHistory.length > 1) {
+				// last element of etsy.requestHistory is the current call, so pop it
+				etsy.requestHistory.pop();
+				// previous call from this page is now last element, so send new request with popped element
+				var previousRequest = etsy.requestHistory.pop();
+				// this request will add it back into history again; next back call will remove it
+				etsy.getRequest(previousRequest.purpose, previousRequest.uri, previousRequest.parameters);
+			}
+		} else if (kc === 16) { // press shift to set focus to search bar
+			$('#search input').focus();
 		}
-		if (etsy.requestHistory.length > 1) {
-			// last element of etsy.requestHistory is the current call, so pop it
-			etsy.requestHistory.pop();
-			// previous call from this page is now last element, so send new request with popped element
-			var previousRequest = etsy.requestHistory.pop();
-			// this request will add it back into history again; next back call will remove it
-			etsy.getRequest(previousRequest.purpose, previousRequest.uri, previousRequest.parameters);
+	} else {
+		if (kc === 13) { // enter to search
+			var searchString = $('#search input').val().replace(/\s+/g, '%20');
+			console.log(searchString);
+			$('#search input').val('');
+			etsy.getRequest('listings', 'listings/active', {'limit': 30, 'keywords': searchString});
+			// remove focus from input so user can use site again
+			$('#search input').blur();
 		}
 	}
+	
 	// cancel speech because cell won't contain same content
 	speechSynthesis.cancel();
 });
@@ -89,3 +105,15 @@ function activateCategoryCell(kc) {
 	var cell = $('.hover-row:first-child .cell:nth-child('+(1+linkListKeyCodes.indexOf(kc))+')');
 	etsy.getRequest('listings', 'listings/active', {'limit': 30, 'category': cell.data('name')});
 }
+
+
+// Search //
+$('#search input').focus(function() {
+	// Message about turning off Fingers
+});
+$('#search form').submit(function(e) {
+	e.preventDefault();
+})
+
+
+
